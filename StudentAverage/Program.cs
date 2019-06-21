9c1dd78c097f9cc5b69f004a3fc82a73899629e7
Jennifer Young <jclyoung@gmail.com>
2019-06-21 16:22:23 -0500

    Completed

--- StudentAverage/Program.cs
+++ StudentAverage/Program.cs
@@ -1,17 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace StudentAverage
 {
     public static class Program
     {
+
         public static void Main()
         {
+            string[] lines = File.ReadAllLines(@"studentdata.txt");
+            List<Student> students = CreateStudent(lines);
             foreach (var student in students)
             {
                 Console.WriteLine($"{student.Name} Average: {student.GetAverage()}");
             }
 
             Console.ReadLine();
+        }
+
+        private static List<Student> CreateStudent(string[] lines)
+        {
+            List<string> students = new List<string>();
+            List<Student> newStudents = new List<Student>();
+            string name = "";
+            int[] scores = Array.Empty<int>();
+
+            foreach (string line in lines)
+            {
+                students.Add(line);
+            }
+
+            foreach (string stud in students)
+            {
+                Student student = new Student(name, scores);
+                string[] data = stud.Split(" ");
+                name = data[0];
+                scores = new int[data.Length - 1];
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    if (i != 0)
+                        scores[i - 1] = Convert.ToInt32(data[i]);
+                }
+
+                student.Name = name;
+                student.Scores = scores;
+                newStudents.Add(student);
+            }
+            return newStudents;
+
         }
     }
 }
--- StudentAverage/Student.cs
+++ StudentAverage/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace StudentAverage
 {
     public class Student
@@ -8,8 +9,34 @@ namespace StudentAverage
 
         public int[] Scores { get; set; }
 
+
+        public Student()
+        {
+            Name = " ";
+            Scores = new int[] { };
+        }
+
+        public Student(string name, int[] scores)
+        {
+            Name = name;
+            Scores = scores;
+        }
+
         public int? GetAverage()
         {
+            
+            int sum = 0;
+            foreach (int score in Scores)
+            {
+                sum += score;
+            }
+
+            double length = Scores.Length;
+            Convert.ToDouble(sum);
+            double average = sum / length;
+            Math.Round(average);
+            int avg = Convert.ToInt32(average);
+            return avg;
         }
     }
 }
